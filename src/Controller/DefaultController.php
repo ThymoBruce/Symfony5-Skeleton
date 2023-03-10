6b0cad1b5c045b0f8b5ef548aff1c7a94e4cbf64
thymobruce <thymoroben@gmail.com>
2023-03-10 15:59:55 +0100

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+        return $this->render('Pages/index.html.twig', [
+
+        ]);
+    }
+}
